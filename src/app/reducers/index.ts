d993ae042314879cc815fb8cfbc417c7efe43cdc
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-12-27 16:17:18 -0200

    Configure layout reducer to app store

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,13 +6,14 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { LayoutState, layoutReducer } from '../layout/layout.reducer';
 
 export interface State {
-
+  layout: LayoutState;
 }
 
 export const reducers: ActionReducerMap<State> = {
-
+  layout: layoutReducer
 };
 
 
