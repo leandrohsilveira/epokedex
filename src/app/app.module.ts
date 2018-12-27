bbcb7daa412f180b157ee15de68d46d585ec705c
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-12-27 14:04:13 -0200

    Configure root reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 
+export const appModuleReducers = {};
+
 export const appModuleImports = [
+  StoreModule.forRoot(appModuleReducers),
+  StoreDevtoolsModule.instrument(),
   LayoutModule,
   NgbModule
 ];
