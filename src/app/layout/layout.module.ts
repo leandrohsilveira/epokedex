3a11c72dabf4e196a3035347ee254649002a9ed6
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-12-27 15:50:10 -0200

    Configure Layout Reducer

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
+import { StoreModule } from '@ngrx/store';
+import * as fromLayout from './layout.reducer';
 
 export const layoutModuleDeclarations = [
   LayoutComponent
 ];
 
+export const layoutModuleImports = [
+  StoreModule.forFeature('layout', fromLayout.reducer)
+];
+
 @NgModule({
   declarations: layoutModuleDeclarations,
   imports: [
-    CommonModule
+    CommonModule,
+    ...layoutModuleImports,
   ],
   exports: [
     LayoutComponent
