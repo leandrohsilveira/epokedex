47f1285dd99d1440a2b5b44d977c8978db36983f
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-12-29 14:16:35 -0200

    Add pokemon list actions

--- src/app/pokemon/pokemon.actions.ts
+++ src/app/pokemon/pokemon.actions.ts
@@ -1,11 +1,19 @@
 import { Action } from '@ngrx/store';
+import { Pokemons } from './pokemon';
 
 export enum PokemonActionTypes {
-  LoadPokemons = '[Pokemon] Load Pokemons'
+  LoadPokemons = '[Pokemon] Load Pokemons',
+  PokemonsLoaded = '[Pokemon] Pokemons loaded'
 }
 
 export class LoadPokemons implements Action {
   readonly type = PokemonActionTypes.LoadPokemons;
 }
 
-export type PokemonActions = LoadPokemons;
+export class PokemonsLoaded implements Action {
+  readonly type = PokemonActionTypes.PokemonsLoaded;
+
+  constructor(public pokemons: Pokemons) {}
+}
+
+export type PokemonActions = LoadPokemons | PokemonsLoaded;
