0226b0cefac4cb212a6295be2b18f934ba22d4f4
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-12-29 15:05:21 -0200

    Add pokemon load side effect (incomplete)

--- src/app/pokemon/pokemon.effects.spec.ts
+++ src/app/pokemon/pokemon.effects.spec.ts
@@ -1,25 +1,49 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { Observable } from 'rxjs';
+import { Observable, Subject, ReplaySubject } from 'rxjs';
 
 import { PokemonEffects } from './pokemon.effects';
+import {
+  PokemonActions,
+  PokemonActionTypes,
+  LoadPokemons,
+  PokemonsLoaded
+} from './pokemon.actions';
 
 describe('PokemonEffects', () => {
-  let actions$: Observable<any>;
+  let actions$: Subject<PokemonActions>;
   let effects: PokemonEffects;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [
-        PokemonEffects,
-        provideMockActions(() => actions$)
-      ]
+      providers: [PokemonEffects, provideMockActions(() => actions$)]
     });
-
+    actions$ = new ReplaySubject();
     effects = TestBed.get(PokemonEffects);
   });
 
   it('should be created', () => {
     expect(effects).toBeTruthy();
+  });
+
+  describe(`on "${PokemonActionTypes.LoadPokemons}" action`, () => {
+    it(`it effects to ${
+      PokemonActionTypes.PokemonsLoaded
+    } action with pokemons list filled`, done => {
+      const action = new LoadPokemons();
+
+      actions$.next(action);
+
+      effects.onLoadPokemons.subscribe((effect: PokemonsLoaded) => {
+        try {
+          expect(effect).toBeTruthy();
+          expect(effect.type).toEqual(PokemonActionTypes.PokemonsLoaded);
+          expect(effect.pokemons).toEqual([{ id: '123', name: 'Pikachu' }]);
+          done();
+        } catch (e) {
+          done.fail(e);
+        }
+      });
+    });
   });
 });
--- src/app/pokemon/pokemon.effects.ts
+++ src/app/pokemon/pokemon.effects.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect } from '@ngrx/effects';
-
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { PokemonActionTypes, PokemonsLoaded } from './pokemon.actions';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class PokemonEffects {
-
   constructor(private actions$: Actions) {}
+
+  @Effect()
+  onLoadPokemons = this.actions$.pipe(
+    ofType(PokemonActionTypes.LoadPokemons),
+    map(action => new PokemonsLoaded([{ id: '123', name: 'Pikachu' }]))
+  );
 }
