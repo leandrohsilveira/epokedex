dccff830c70f9110f85990c17e72462b9fec09aa
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-12-29 14:17:54 -0200

    Add pokemon reducer tests

--- src/app/pokemon/pokemon.reducer.spec.ts
+++ src/app/pokemon/pokemon.reducer.spec.ts
@@ -1,4 +1,10 @@
 import { reducer, initialState } from './pokemon.reducer';
+import {
+  PokemonActionTypes,
+  LoadPokemons,
+  PokemonsLoaded
+} from './pokemon.actions';
+import { Pokemons } from './pokemon';
 
 describe('Pokemon Reducer', () => {
   describe('an unknown action', () => {
@@ -8,6 +14,35 @@ describe('Pokemon Reducer', () => {
       const result = reducer(initialState, action);
 
       expect(result).toBe(initialState);
+    });
+  });
+
+  describe(`a "${PokemonActionTypes.LoadPokemons}" action`, () => {
+    it('to a state with loading "true"', () => {
+      const action = new LoadPokemons();
+
+      const result = reducer(initialState, action);
+
+      expect(result).toEqual({
+        ...initialState,
+        loading: true
+      });
+    });
+  });
+
+  describe(`a ${PokemonActionTypes.PokemonsLoaded} action`, () => {
+    it('to a state with loading "false" and pokemons array filled', () => {
+      const pokemons: Pokemons = [
+        {
+          id: '123',
+          name: 'Pikachu'
+        }
+      ];
+      const action = new PokemonsLoaded(pokemons);
+
+      const result = reducer({ ...initialState, loading: true }, action);
+
+      expect(result).toEqual({ ...initialState, loading: false, pokemons });
     });
   });
 });
