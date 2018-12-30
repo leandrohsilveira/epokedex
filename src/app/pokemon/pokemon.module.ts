86625c07a41a8f07d69b79887b418a90b133a539
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-12-30 00:00:18 -0200

    Configure http client module

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -1,23 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { PokemonRoutingModule } from './pokemon-routing.module';
 import { StoreModule } from '@ngrx/store';
 import * as fromPokemon from './pokemon.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { PokemonEffects } from './pokemon.effects';
+import { PokemonService } from './pokemon.service';
+import { HttpClientModule } from '@angular/common/http';
 
 export const pokemonModuleImports = [
   StoreModule.forFeature('pokemon', fromPokemon.reducer),
   EffectsModule.forFeature([PokemonEffects])
 ];
 
+export const pokemonModuleProviders = [PokemonService];
+
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    HttpClientModule,
     PokemonRoutingModule,
     ...pokemonModuleImports
   ]
+  // providers: [...pokemonModuleProviders]
 })
-export class PokemonModule { }
+export class PokemonModule {}
