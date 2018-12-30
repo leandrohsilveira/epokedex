62ce2e9a42467ef127912907b7981d11ddc094d5
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-12-29 23:59:57 -0200

    Add pokeapi entities

--- src/app/pokemon/pokemon.ts
+++ src/app/pokemon/pokemon.ts
@@ -3,4 +3,18 @@ export interface Pokemon {
   name: string;
 }
 
+export interface PokeApiList<T> {
+  count: number;
+  next?: string;
+  previous?: string;
+  results: T[];
+}
+
+export interface PokeApiPageable {
+  offset?: number;
+  limit?: number;
+}
+
+export interface PokeApiPokemonList extends PokeApiList<Pokemon> {}
+
 export type Pokemons = Pokemon[];
