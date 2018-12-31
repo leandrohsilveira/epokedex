425f3b9310a21cb3f290076fd34f475ee75fbf8b
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-12-31 15:26:13 -0200

    Add pokemon list page component

---
+++ src/app/pokemon/pokemon-list-page/pokemon-list-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-list-page',
+  templateUrl: './pokemon-list-page.component.html',
+  styleUrls: ['./pokemon-list-page.component.css']
+})
+export class PokemonListPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pokemon/pokemon-routing.module.ts
+++ src/app/pokemon/pokemon-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PokemonListPageComponent } from './pokemon-list-page/pokemon-list-page.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [PokemonListPageComponent]
 })
 export class PokemonRoutingModule {}
