724db700e136497837a8a851ead5021cde1ace59
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-12-31 11:32:08 -0200

    Improve PokemonsLoaded action reducer tests

--- src/app/pokemon/pokemon.reducer.spec.ts
+++ src/app/pokemon/pokemon.reducer.spec.ts
@@ -31,18 +31,28 @@ describe('Pokemon Reducer', () => {
   });
 
   describe(`a ${PokemonActionTypes.PokemonsLoaded} action`, () => {
-    it('to a state with loading "false" and pokemons array filled', () => {
-      const pokemons: PokeApiNamedResource[] = [
-        {
-          name: 'bulbasaur',
-          url: 'https://pokeapi.co/api/v2/pokemon/1/'
-        }
-      ];
+    const pokemons: PokeApiNamedResource[] = [
+      {
+        name: 'bulbasaur',
+        url: 'https://pokeapi.co/api/v2/pokemon/1/'
+      }
+    ];
+
+    let result;
+
+    beforeEach(() => {
       const action = new PokemonsLoaded(pokemons);
+      result = reducer({ ...initialState, loading: true }, action);
+    });
 
-      const result = reducer({ ...initialState, loading: true }, action);
+    it('to a state with loading "false"', () => {
+      expect(result).toBeTruthy();
+      expect(result.loading).toBeFalsy();
+    });
 
-      expect(result).toEqual({ ...initialState, loading: false, pokemons });
+    it('to a state with pokemons array filled', () => {
+      expect(result).toBeTruthy();
+      expect(result.pokemons).toEqual(pokemons);
     });
   });
 });
