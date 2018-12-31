cf3aea7031106915368155aa2c71381969542a37
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-12-31 15:27:01 -0200

    Import LayoutModule into PokemonRoutingModule

--- src/app/pokemon/pokemon-routing.module.ts
+++ src/app/pokemon/pokemon-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PokemonListPageComponent } from './pokemon-list-page/pokemon-list-page.component';
+import { LayoutModule } from '../layout/layout.module';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), LayoutModule],
   exports: [RouterModule],
   declarations: [PokemonListPageComponent]
 })
