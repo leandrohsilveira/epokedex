e5b5c6fd807780b1e4d6fa8d271ca539a5b8e0b3
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-01 20:38:48 -0200

    Add first draft of pokemon list page component implementation

--- src/app/pokemon/pokemon-list-page/pokemon-list-page.component.spec.ts
+++ src/app/pokemon/pokemon-list-page/pokemon-list-page.component.spec.ts
@@ -3,10 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PokemonListPageComponent } from './pokemon-list-page.component';
 import { LayoutModule } from 'src/app/layout/layout.module';
 import { PokemonListComponent } from '../pokemon-list/pokemon-list.component';
-import {
-  pokemonModuleImports,
-  pokemonModuleProviders
-} from '../pokemon.module';
+import { pokemonModuleImports } from '../pokemon.module';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
@@ -24,7 +21,7 @@ describe('PokemonListPageComponent', () => {
         LayoutModule,
         ...pokemonModuleImports
       ],
-      providers: [...pokemonModuleProviders],
+      providers: [],
       declarations: [PokemonListComponent, PokemonListPageComponent]
     }).compileComponents();
   }));
--- src/app/pokemon/pokemon-list-page/pokemon-list-page.component.ts
+++ src/app/pokemon/pokemon-list-page/pokemon-list-page.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store, select } from '@ngrx/store';
-import { PokemonFeatureState, pokemonListSelector } from '../pokemon.reducer';
+import {
+  PokemonFeatureState,
+  pokemonListSelector,
+  pokemonCountSelector
+} from '../pokemon.reducer';
 import { LoadPokemons } from '../pokemon.actions';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { PokeApiNamedResource, PokeApiPageable } from '../pokeapi';
-import { withLatestFrom, map, takeWhile } from 'rxjs/operators';
+import { withLatestFrom, map, takeWhile, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pokemon-list-page',
@@ -16,11 +20,16 @@ export class PokemonListPageComponent implements OnInit, OnDestroy {
 
   mounted = true;
   pokemons$: Observable<PokeApiNamedResource[]>;
+  count$: Observable<number>;
   offset$ = new BehaviorSubject(0);
   limit$ = new BehaviorSubject(10);
+  page$ = this.getPageable().pipe(
+    map(pageable => this.calculatePage(pageable))
+  );
 
   ngOnInit() {
     this.pokemons$ = this.store$.pipe(select(pokemonListSelector));
+    this.count$ = this.store$.pipe(select(pokemonCountSelector));
     this.getPageable()
       .pipe(takeWhile(() => this.mounted))
       .subscribe(pageable => this.loadPokemons(pageable));
@@ -28,6 +37,14 @@ export class PokemonListPageComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.mounted = false;
+  }
+
+  setPage(page: number): void {
+    this.getPageable()
+      .pipe(take(1))
+      .subscribe(({ limit }) => {
+        this.offset$.next(page * limit - limit);
+      });
   }
 
   private getPageable(): Observable<PokeApiPageable> {
@@ -39,5 +56,10 @@ export class PokemonListPageComponent implements OnInit, OnDestroy {
 
   private loadPokemons(pageable: PokeApiPageable): void {
     this.store$.dispatch(new LoadPokemons(pageable));
+  }
+
+  private calculatePage({ offset, limit }: PokeApiPageable): number {
+    const pageCount = Math.floor(offset / limit);
+    return offset % limit === 0 ? pageCount : pageCount + 1;
   }
 }
