596ba235cfcc880a5d68a7e3a751bfc2dbbe4244
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-01 20:37:35 -0200

    Add pokemon list page to a route in PokemonRoutingModule

--- src/app/pokemon/pokemon-routing.module.ts
+++ src/app/pokemon/pokemon-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PokemonListPageComponent } from './pokemon-list-page/pokemon-list-page.component';
 import { LayoutModule } from '../layout/layout.module';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: PokemonListPageComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes), LayoutModule],
-  exports: [RouterModule],
+  imports: [RouterModule.forChild(routes), LayoutModule, NgbPaginationModule],
+  exports: [RouterModule, PokemonListPageComponent],
   declarations: [PokemonListPageComponent]
 })
 export class PokemonRoutingModule {}
