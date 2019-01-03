703e0c7e8848789559e1953d3dd989bbb607c444
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-03 19:47:08 -0200

    Implement pokemon favorite list page

--- src/app/pokemon/pokemon-favorite-list-page/pokemon-favorite-list-page.component.ts
+++ src/app/pokemon/pokemon-favorite-list-page/pokemon-favorite-list-page.component.ts
@@ -1,12 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import {
+  PokemonFeatureState,
+  pokemonFavoritePokemonsSelector,
+  pokemonFavoritesLoadedSelector,
+  pokemonLoadingFavoritesSelector,
+  pokemonFavoriteCountSelector
+} from '../pokemon.reducer';
+import { Observable, BehaviorSubject, combineLatest } from 'rxjs';
+import { Pokemon } from '../pokeapi';
+import { map, mergeMap, takeWhile, filter, take } from 'rxjs/operators';
+import { LoadFavoritePokemons } from '../pokemon.actions';
 
 @Component({
   selector: 'app-pokemon-favorite-list-page',
   templateUrl: './pokemon-favorite-list-page.component.html',
   styleUrls: ['./pokemon-favorite-list-page.component.scss']
 })
-export class PokemonFavoriteListPageComponent implements OnInit {
-  constructor() {}
+export class PokemonFavoriteListPageComponent implements OnInit, OnDestroy {
+  constructor(private store$: Store<PokemonFeatureState>) {}
 
-  ngOnInit() {}
+  mounted = true;
+  pokemons$: Observable<Pokemon[]>;
+  count$: Observable<number>;
+  loaded$: Observable<boolean>;
+  loading$: Observable<boolean>;
+  page$ = new BehaviorSubject(1);
+  limit$ = new BehaviorSubject(10);
+
+  ngOnInit() {
+    this.loaded$ = this.store$.pipe(select(pokemonFavoritesLoadedSelector));
+    this.loading$ = this.store$.pipe(select(pokemonLoadingFavoritesSelector));
+    this.count$ = this.store$.pipe(select(pokemonFavoriteCountSelector));
+    this.pokemons$ = combineLatest(this.page$, this.limit$).pipe(
+      map(([page, limit]) => ({
+        offset: limit * page - limit,
+        limit
+      })),
+      mergeMap(pageable =>
+        this.store$.pipe(select(pokemonFavoritePokemonsSelector, pageable))
+      )
+    );
+
+    combineLatest(this.loaded$, this.loading$)
+      .pipe(
+        takeWhile(() => this.mounted),
+        filter(([loaded, loading]) => {
+          console.log('Load favorite pokemons pipe filter', loaded, loading);
+          return !loaded && !loading;
+        })
+      )
+      .subscribe(() => this.store$.dispatch(new LoadFavoritePokemons()));
+  }
+
+  ngOnDestroy() {
+    this.mounted = false;
+  }
+
+  setPage(page: number) {
+    this.page$.next(page);
+  }
+
+  handlePokemonClick(pokemon: Pokemon) {
+    console.log('Clicked to view favorite pokemon', pokemon);
+  }
 }
