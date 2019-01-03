f5ed45351cb34a2246bce692d7aa2d967e5c5231
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-03 14:18:07 -0200

    Add favorite pokemons actions

--- src/app/pokemon/pokemon.actions.ts
+++ src/app/pokemon/pokemon.actions.ts
@@ -1,9 +1,11 @@
 import { Action } from '@ngrx/store';
-import { PokeApiNamedResource, PokeApiPageable, Pokemon } from './pokeapi';
+import { PokeApiPageable, Pokemon } from './pokeapi';
 
 export enum PokemonActionTypes {
   LoadPokemons = '[Pokemon] Load Pokemons',
-  PokemonsLoaded = '[Pokemon] Pokemons loaded'
+  PokemonsLoaded = '[Pokemon] Pokemons loaded',
+  LoadFavoritePokemons = '[Pokemon] Load favorite pokemons',
+  FavoritePokemonsLoaded = '[Pokemon] Favorite pokemons loaded'
 }
 
 export class LoadPokemons implements Action {
@@ -18,4 +20,18 @@ export class PokemonsLoaded implements Action {
   constructor(public pokemons: Pokemon[], public count: number) {}
 }
 
-export type PokemonActions = LoadPokemons | PokemonsLoaded;
+export class LoadFavoritePokemons implements Action {
+  readonly type = PokemonActionTypes.LoadFavoritePokemons;
+}
+
+export class FavoritePokemonsLoaded implements Action {
+  readonly type = PokemonActionTypes.FavoritePokemonsLoaded;
+
+  constructor(public favoritePokemons: Pokemon[]) {}
+}
+
+export type PokemonActions =
+  | LoadPokemons
+  | PokemonsLoaded
+  | LoadFavoritePokemons
+  | FavoritePokemonsLoaded;
