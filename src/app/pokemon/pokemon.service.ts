3c871aa510f90dffe22c6e343687d1bcbab3eed5
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-03 16:02:19 -0200

    Add PokemonService favorites functions

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { PokeApiPokemonList, PokeApiPageable } from './pokeapi';
-import { Observable } from 'rxjs';
+import { PokeApiPokemonList, PokeApiPageable, Pokemon } from './pokeapi';
+import { Observable, of } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Injectable()
 export class PokemonService {
   constructor(private http: HttpClient) {}
+
+  static FAVORITE_POKEMONS_KEY = 'ng.app.epokedex.favoritePokemons';
 
   findAll(
     pageable: PokeApiPageable = { offset: 0, limit: 10 }
@@ -13,6 +16,19 @@ export class PokemonService {
     const { offset = 0, limit = 10 } = pageable;
     return this.http.get<PokeApiPokemonList>(
       `/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    );
+  }
+
+  restoreFavorites(): Observable<Pokemon[]> {
+    return of(
+      window.localStorage.getItem(PokemonService.FAVORITE_POKEMONS_KEY)
+    ).pipe(map(str => (str ? JSON.parse(str) : [])));
+  }
+
+  storeFavorites(pokemons: Pokemon[]) {
+    window.localStorage.setItem(
+      PokemonService.FAVORITE_POKEMONS_KEY,
+      JSON.stringify(pokemons)
     );
   }
 }
