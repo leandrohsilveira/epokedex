a62f914588ca63843009b4f3e6132686ced9e9a7
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-04 19:35:51 -0200

    Dispatch error message on pokemon detail fetch error

--- src/app/pokemon/pokemon-detail-page/pokemon-detail-page.component.ts
+++ src/app/pokemon/pokemon-detail-page/pokemon-detail-page.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../pokemon.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 import { PokemonDetail } from '../pokeapi';
-import { switchMap, take, map } from 'rxjs/operators';
+import { switchMap, take, catchError } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Store } from '@ngrx/store';
+import { PokemonFeatureState } from '../pokemon.reducer';
+import { PushMessage } from 'src/app/layout/layout.actions';
 
 @Component({
   selector: 'app-pokemon-detail-page',
@@ -14,6 +18,7 @@ export class PokemonDetailPageComponent implements OnInit {
   constructor(
     private pokemonService: PokemonService,
     private router: Router,
+    private store$: Store<PokemonFeatureState>,
     private route: ActivatedRoute
   ) {}
 
@@ -25,9 +30,13 @@ export class PokemonDetailPageComponent implements OnInit {
     this.route.paramMap
       .pipe(
         take(1),
-        switchMap(params => this.pokemonService.findOne(params.get('name')))
+        switchMap(params => this.pokemonService.findOne(params.get('name'))),
+        catchError((error: HttpErrorResponse) => {
+          this.store$.dispatch(PushMessage.danger(error.statusText));
+          return of(error);
+        })
       )
-      .subscribe(pokemon => {
+      .subscribe((pokemon: PokemonDetail) => {
         this.loading = false;
         this.pokemon = pokemon;
         this.title$.next(`Details of ${pokemon.name}`);
