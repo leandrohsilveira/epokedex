0df2763f5f2a6948c093f9e2fefbebb39e1fe45a
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-04 01:23:09 -0200

    Fix PokemonDetailPage create component test

--- src/app/pokemon/pokemon-detail-page/pokemon-detail-page.component.spec.ts
+++ src/app/pokemon/pokemon-detail-page/pokemon-detail-page.component.spec.ts
@@ -1,6 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PokemonDetailPageComponent } from './pokemon-detail-page.component';
+import {
+  pokemonModuleImports,
+  pokemonModuleProviders
+} from '../pokemon.module';
+import { PokemonService } from '../pokemon.service';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LayoutModule } from 'src/app/layout/layout.module';
 
 describe('PokemonDetailPageComponent', () => {
   let component: PokemonDetailPageComponent;
@@ -8,9 +18,17 @@ describe('PokemonDetailPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PokemonDetailPageComponent ]
-    })
-    .compileComponents();
+      imports: [
+        StoreModule.forRoot({}),
+        EffectsModule.forRoot([]),
+        HttpClientTestingModule,
+        RouterTestingModule,
+        LayoutModule,
+        ...pokemonModuleImports
+      ],
+      providers: [...pokemonModuleProviders],
+      declarations: [PokemonDetailPageComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
